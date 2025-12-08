cf8349cfacdbf4f95c5ce592e0b97ebc3c21ec0e
Marc <marcgomeztejedor@gmail.com>
2025-12-08 13:34:02 +0100

    cleaned up a bit some code for the animator and added 2 new cool wave funcs for movement

--- Assets/Scripts/Animations/YMover.cs
+++ Assets/Scripts/Animations/YMover.cs
@@ -11,13 +11,13 @@ public class YMover : MonoBehaviour
     public void SetX(float value)
     {
         Vector3 p = transform.localPosition;
-        p.x = 1.5f + value*0.5f;
+        p.x = value;
         transform.localPosition = p;
     }
     public void SetZ(float value)
     {
         Vector3 p = transform.localPosition;
-        p.z = 1.5f + value*0.5f;
+        p.z = value;
         transform.localPosition = p;
     }
 }
